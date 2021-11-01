4c1e3f61329d40986d61a235a7626f796b51e8ab
binh <dotranbinhqng02@gmail.com>
2021-11-01 17:25:25 +0700

    done

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 function plus(a, b) {
-    let c = parseInt(a,10) + parseInt(b,10);
+    let c = parseFloat(a,10) + parseFloat(b,10);
     document.getElementById("resultPlus").innerText = c;
 }
 
 function subtract(a, b) {
-    let c = parseInt(a,10) - parseInt(b,10);
+    let c = parseFloat(a,10) - parseFloat(b,10);
     document.getElementById("resultSubtract").innerText = c;
 }
 
 
 function multipli(a, b) {
-    let c = parseInt(a,10) * parseInt(b,10);
+    let c = parseFloat(a,10) * parseFloat(b,10);
     document.getElementById("resultMultipli").innerText = c;
 }
 
 
 function division(a, b) {
-    let c = parseInt(a,10) / parseInt(b,10);
+    if (b!=0) {
+        let c = parseFloat(a,10) / parseFloat(b,10);
     document.getElementById("resultDivis").innerText = c;
+    }else{document.getElementById("resultDivis").innerHTML = "Nhập b # 0."}
 }
 
