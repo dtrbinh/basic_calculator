4d243f64e3abb2ee6d1bb455e0b52c7a1537fd63
binh <dotranbinhqng02@gmail.com>
2021-11-01 13:39:51 +0700

    coding...

---
+++ main.js
@@ -0,0 +1,22 @@
+function plus(a, b) {
+    let c = parseInt(a,10) + parseInt(b,10);
+    document.getElementById("resultPlus").innerText = c;
+}
+
+function subtract(a, b) {
+    let c = parseInt(a,10) - parseInt(b,10);
+    document.getElementById("resultSubtract").innerText = c;
+}
+
+
+function multipli(a, b) {
+    let c = parseInt(a,10) * parseInt(b,10);
+    document.getElementById("resultMultipli").innerText = c;
+}
+
+
+function division(a, b) {
+    let c = parseInt(a,10) / parseInt(b,10);
+    document.getElementById("resultDivis").innerText = c;
+}
+
